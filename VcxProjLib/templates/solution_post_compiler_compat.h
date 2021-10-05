968b759adc2d0f8f46e20317fafbf2d4cce0b96c
Alexey Suslov <alexey.s+git@ultibot.ru>
2021-10-05 20:24:35 +0700

    Updated compiler compat to the latest version.

--- VcxProjLib/templates/solution_post_compiler_compat.h
+++ VcxProjLib/templates/solution_post_compiler_compat.h
@@ -21,21 +21,20 @@
  */
 
 #define __PRETTY_FUNCTION__ __func__
+
+#define thread_local
+#define restrict
+
 #define __extension__
 #define __attribute__(...)
 #define __attribute(...)
+
 #define asm __asm__
 #define __asm __asm__
 
-#if !defined(typeof)
-#define __typeof__ int
-#define typeof __typeof__
-#define __typeof __typeof__
-#endif
-
-#ifndef container_of
-#define container_of(ptr, type, member) ((type*)(ptr))
-#endif
+//#define __typeof__(a) int
+//#define __typeof(a) __typeof__(a)
+//#define typeof(a) __typeof__(a)
 
 /*
  * Builtins and so on
@@ -45,14 +44,15 @@
 // Simplified but syntactically identical
 //
 #define __atomic_load_n(ptr, memorder) (*(ptr))
-#define __atomic_store_n(ptr, val, memorder) (*(ptr) = val)
+#define __atomic_store_n(ptr, val, memorder) (*(ptr) = (val))
 
 #define __sync_fetch_and_add(ptr, amount) (*(ptr) + (amount))
 #define __sync_fetch_and_and(ptr, amount) (*(ptr) & (amount))
 
 #define __builtin_expect(exp, likely) (exp)
 #define __builtin_bswap16(value) ((((value) & 0xff) << 8) | (((value) >> 8) & 0xff))
-#define __builtin_bswap32(value) ((((value >> 0) & 0xff) << 24) | (((value >> 8) & 0xff) << 16) | (((value >> 16) & 0xff) << 8) | (((value >> 24) & 0xff) << 0))
+#define __builtin_bswap32(value) ((__builtin_bswap16((value) & 0xffff) << 16) | __builtin_bswap16((value) >> 16))
+#define __builtin_bswap64(value) ((__builtin_bswap32((value) & 0xffffffff) << 32) | __builtin_bswap32((value) >> 32))
 
 #define __builtin_offsetof(t, m) ((__INTPTR_TYPE__)(&(((t*)0)->m)))
 
@@ -61,7 +61,6 @@
 //
 // Stubs; for example, dependent on typeof which still not supported. Or just lazy :)
 //
-#define __builtin_offsetof(type, member) 0
 #define __builtin_types_compatible_p(t1, t2) 1
 
 #define __builtin_return_address(n) 0
@@ -75,6 +74,7 @@
 #define __builtin_mul_overflow_p(a, b, c) 0
 
 #define __builtin_clz(value) 0
+#define __builtin_ctz(value) 0
 #define __builtin_ctzl(value) 0
 #define __builtin_ctzll(value) 0
 #define __builtin_ffs(value) 0
@@ -94,3 +94,7 @@
 #define __sync_lock_release(lock)
 
 #define __builtin_trap()
+
+/*
+ * User defined
+ */
